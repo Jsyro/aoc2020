754da3ce3bbb6c03c4ed9ae8708eeaedfc185a34
Jason <jasyrotuck@gmail.com>
2020-12-07 23:33:18 -0800

    14 stars

--- 2020/day7.py
+++ 2020/day7.py
@@ -22,40 +22,42 @@ for line in lines:
 
 all_parents = set()
 def find_parent_tree(init_bag, contents, depth):
-    print('starting bag')
-    print(init_bag)
+    # print('starting bag')
+    # print(init_bag)
     one_level_up_parents = set()
     for k,v in contents.items():
         if init_bag in v.keys():
-            print('found_parent')
+            # print('found_parent')
             one_level_up_parents.add(k)
             all_parents.add(k)
     if one_level_up_parents:
-        print('take another lap')
+        # print('take another lap')
         for p in one_level_up_parents:
-            print(p)
-            print('find all parents of')
+            # print(p)
+            # print('find all parents of')
             find_parent_tree(p,contents,depth+1)
     else:
-        print('all done')
+        pass
+        # print('all done')
     return
 
 find_parent_tree('shiny gold',contents,1)
-print(all_parents)
+# print(all_parents)
 print(len(all_parents))
 
-# def find_all_parents(set_of_bags, contents):
-#     print(set_of_bags)
-#     init_size = len(set_of_bags)
-#     for k,v in contents.items(): 
-#         if any(i in set_of_bags for i in set(v.keys())):
-#             set_of_bags.add(k)
-#     new_size = len(set_of_bags)
-#     if init_size != new_size:
-#         set_of_bags = find_all_parents(set_of_bags, contents)
-#     return set_of_bags
+# part 2
 
-# bags = set(['shiny gold'])
-# bags = find_all_parents(bags,contents)
+def count_all_children(bag,contents,depth):
+    if bag not in contents.keys():
+        return 0
+    count=0
+    for b,cnt in contents[bag].items():
+        num_children = 0
+        print(b + ":" + str(cnt))
+        num_children = (count_all_children(b,contents,depth+1)+1)*cnt
+        print(b + ":" + str(cnt) + ">>" + str(num_children))
+        count += num_children
+        print (count)
+    return count
 
-# print(len(bags))
+print(count_all_children('shiny gold',contents,1))
